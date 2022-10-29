2a3101dc69a6ef9f04106023b4ab51148aa3d598
teedaa <pengtedachengllhs@gmail.com>
2022-10-28 22:13:11 -0700

    Give up on delete function

--- server.js
+++ server.js
@@ -52,37 +52,6 @@ app.post('/api/notes', (req, res) => {
       });
     }
     });
-
-    //Note delete function
-    app.delete('/api/notes', (req, res) => {
-        var del = req.params.id;
-      
-          fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-              console.error(err);
-            } else {
-              const parsedNotes = JSON.parse(data);
-            for (i = 0; i < parsedNotes.length; i++){
-                if (del == parsedNotes[i].id){
-                    parsedNotes.splice(i, 1)
-                }
-                parsedNotes.splice(newNote);
-                dbNotes = parsedNotes;
-                fs.writeFile(
-                    './db/db.json',
-                    JSON.stringify(parsedNotes, null, 3),
-                    (writeErr) =>
-                    writeErr
-                    ? console.error(writeErr)
-                    : console.info('Successfully updated your notes!')
-                    );
-                }
-            }
-                res.json(dbNotes);
-            
-          });
-        }
-        );
     
     
 app.get('/notes', (req, res) => {
